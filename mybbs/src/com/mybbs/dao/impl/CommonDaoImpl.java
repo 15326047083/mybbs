574578be25931d21f8d1591bfa565e9ef848f663
leiyuan <leiyuan@192.168.1.108>
2018-04-18 20:27:57 +0800

    重新提交

--- mybbs/src/com/mybbs/dao/impl/CommonDaoImpl.java
+++ mybbs/src/com/mybbs/dao/impl/CommonDaoImpl.java
@@ -9,12 +9,17 @@ public class CommonDaoImpl<T> implements CommonDao<T> {
 		System.out.println(common.getClass().getFields());
 		System.out.println(common.getClass().getDeclaredFields());
 		System.out.println(common.getClass().getSimpleName());
+		Class clazz = Class.forName("");// 根据类名获得其对应的Class对象 写上你想要的类名就是了 注意是全名 如果有包的话要加上 比如java.Lang.String
+		Field[] fields = clazz.getDeclaredFields();// 根据Class对象获得属性 私有的也可以获得
+		for (Field f : fields) {
+			System.out.println(f.getType().getName());// 打印每个属性的类型名字
+		}
 	}
 
 	@Override
 	public void updateCommon(T common) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
