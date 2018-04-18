d21c142845a23b2561a49698b40943adc018c172
leiyuan <leiyuan@10.52.110.81>
2018-04-18 13:24:38 +0800

    萨达萨达

--- MyFilter/src/com/filter/MyFilter.java
+++ MyFilter/src/com/filter/MyFilter.java
@@ -20,7 +20,7 @@ public class MyFilter implements Filter {
 	}
 
 	public void destroy() {
-	}嗷嗷
+	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
