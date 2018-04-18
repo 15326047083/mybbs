49ca785b1572613a75620047926c6040d245cd2e
Administrator <Administrator@Administrator>
2018-04-18 20:16:51 +0800

    cyj

--- mybbs/src/com/mybbs/test/Test.java
+++ mybbs/src/com/mybbs/test/Test.java
@@ -4,21 +4,21 @@ import com.mybbs.dao.CommonDao;
 import com.mybbs.po.Area;
 import com.mybbs.po.User;
 
-public class Test {
-	private CommonDao<User> commonDao;
-	@org.junit.Test
-	public void test() {
-		commonDao=new CommonDao<User>() {
-			
-			@Override
-			public void newCommon(User common) {
-				// TODO Auto-generated method stub
-				System.out.println(common.getClass().toString());
-			}
-		};
-		User u=new User();
-		u.setId(1);
-		commonDao.newCommon(u);
-	}
-}
+//public class Test {
+//	private CommonDao<User> commonDao;
+//	@org.junit.Test
+//	public void test() {
+//	commonDao=new CommonDao<User>() {
+//			
+//			@Override
+//			public void newCommon(User common) {
+//				// TODO Auto-generated method stub
+//				System.out.println(common.getClass().toString());
+//			}
+//		};
+//		User u=new User();
+//		u.setId(1);
+//		commonDao.newCommon(u);
+//	}
+//}
 
