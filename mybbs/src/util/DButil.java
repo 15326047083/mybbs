d3316ca0c28bdfa7ef8571d018d7276da3108360
leiyuan <leiyuan@192.168.1.101>
2018-04-20 10:18:38 +0800

    修改数据库url

--- mybbs/src/util/DButil.java
+++ mybbs/src/util/DButil.java
@@ -7,23 +7,25 @@ import java.sql.SQLException;
 public class DButil {
 
 	private static DButil instance = new DButil();
-	
+
 	private Connection connection;
-	
-	private DButil(){}
-	
-	public static DButil getInstance(){
+
+	private DButil() {
+	}
+
+	public static DButil getInstance() {
 		return instance;
 	}
-	
-	public Connection getConnetion() throws SQLException{
-		connection=DriverManager.getConnection("jdbc:mysql://10.0.61.2:3306/mybbs", "root", "123456");
+
+	public Connection getConnetion() throws SQLException {
+		connection = DriverManager.getConnection(
+				"jdbc:mysql://10.0.61.2:3306/mybbs?useUnicode=true&characterEncoding=utf8", "root", "123456");
 		return connection;
 	}
-	
-	public void closeConnection(){
+
+	public void closeConnection() {
 		try {
-			if(!connection.isClosed()){
+			if (!connection.isClosed()) {
 				connection.close();
 			}
 		} catch (SQLException e) {
@@ -31,18 +33,15 @@ public class DButil {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	static{
+
+	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
+
 }
