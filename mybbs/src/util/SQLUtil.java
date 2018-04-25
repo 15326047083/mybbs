b0f0539e505ed53f6b9bad0722c377916b1215c6
windows <windows@pc>
2018-04-25 18:07:56 +0800

    李学鹏：新增发布公告语句

--- mybbs/src/util/SQLUtil.java
+++ mybbs/src/util/SQLUtil.java
@@ -14,6 +14,7 @@ public class SQLUtil {
 	public static String newPlate = "insert into plate values(null,?,?,?,?,?)";
 	public static String newPost = "insert into post values(null,?,?,?,?,?,?,?)";
 	public static String newReply = "insert into reply values(null,?,?,?,?)";
+	public static String newnotice = "insert into message values(null,?)";
 	public static String updateArea = "update area set name =?,info=?,plateNum=? where id=?";
 	public static String updateUser = "update user set name =?,email=?,password=?,sex=?,age=?,birthday=?,plateId=?,time=?,power=? where id=?";
 	public static String updatePlate = "update plate set name =?,info=?,areaId=?,postNum=?,userId=? where id=?";
@@ -24,9 +25,12 @@ public class SQLUtil {
 	public static String deletePlate = "delete from plate where id=?";
 	public static String deletePost = "delete from post where id=?";
 	public static String deleteReply = "delete from reply where id=?";
+	public static String deletenotice = "delete from message where id=?";
 	public static String getByIdFirstSql = "select * from ";
 	public static String getByIdSecondSql = " where id=?";
 	public static String getListFirstSql = "select * from ";
 	public static String getListSecondSql = " limit ?,20";
 	public static String firstCountSql = "select count(*) from ";
+	
+
 }
