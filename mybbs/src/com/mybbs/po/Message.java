62dbf0446e2b1ac375e42e9f648219b1a20a47fa
windows <windows@pc>
2018-04-25 18:01:15 +0800

    李学鹏：发布信息bean

--- mybbs/src/com/mybbs/po/Message.java
+++ mybbs/src/com/mybbs/po/Message.java
@@ -7,5 +7,42 @@ package com.mybbs.po;
  *
  */
 public class Message {
+	private int id;
+	private String info;
+	
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+	
 
+	public Message(int id, String info) {
+		super();
+		this.id = id;
+		this.info = info;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", info=" + info + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	
 }
