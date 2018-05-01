9644ded9aceb2bb5d9adf9f85a97cdc9fa955dd6
Administrator <Administrator@admin-PC>
2018-05-01 16:07:21 +0800

    李学鹏：模糊查询 不会写 - -

--- mybbs/WebContent/WEB-INF/pages/head/head.jsp
+++ mybbs/WebContent/WEB-INF/pages/head/head.jsp
@@ -46,7 +46,7 @@
 				</span>
 				<div align="left" class="w_search">
 					<div class="w_searchbox">
-						<form action="" method="post">
+						<form action="serachPostServlet" method="post">
 							<input type="hidden" name="path" value="" /> <input type="text"
 								name="queryStr" placeholder="search" /> <input type="submit"
 								value="搜索" />
---
+++ mybbs/src/com/mybbs/servlet/post/serachPostServlet.java
@@ -0,0 +1,72 @@
+package com.mybbs.servlet.post;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class serachPost
+ */
+public class serachPostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public serachPostServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		 try {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+				String queryStr = request.getParameter("queryStr");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://10.0.61.2:3306/mybbs?useUnicode=true&characterEncoding=utf8", "root", "123456");
+				String sql = "select * from post where title like %queryStr%";
+				Statement state = conn.createStatement();
+				 ResultSet rs = state.executeQuery(sql);
+				  while(rs.next()){
+					  String find = rs.getString("title");
+					  System.out.println(find);
+				  }
+				  rs.close();
+				  conn.close();
+				  state.close();
+				  
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 
+		} 
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
